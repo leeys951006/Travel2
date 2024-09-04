31a195792a299a7565917b72f3d4678a148d089c
이연승 <leeys951006@gmail.com>
2024-09-04 11:05:21 +0900

    LoginButton.tsx 수정

--- travelplan/components/LoginButton.tsx
+++ travelplan/components/LoginButton.tsx
@@ -1,21 +1,26 @@
-// components/LoginButton.tsx
-'use client';
-
-import { useRouter } from 'next/navigation';
-
 export default function LoginButton() {
-  const router = useRouter();
+  const handleNaverLogin = () => {
+    window.location.href = '/api/auth/naver';
+  };
 
-  const handleLogin = () => {
-    router.push('/login'); // 로그인 페이지로 이동
+  const handleKakaoLogin = () => {
+    window.location.href = '/api/auth/kakao';
   };
 
   return (
-    <button
-      onClick={handleLogin}
-      className="text-[#1B6AB3] border border-[#1B6AB3] py-2 px-6 rounded-full text-sm md:text-base hover:bg-[#1B6AB3] hover:text-white transition-colors duration-300 ease-in-out"
-    >
-      로그인
-    </button>
+    <div>
+      <button
+        onClick={handleNaverLogin}
+        className="text-[#1B6AB3] border border-[#1B6AB3] py-2 px-6 rounded-full text-sm md:text-base hover:bg-[#1B6AB3] hover:text-white transition-colors duration-300 ease-in-out mb-2"
+      >
+        네이버로 로그인
+      </button>
+      <button
+        onClick={handleKakaoLogin}
+        className="text-[#1B6AB3] border border-[#1B6AB3] py-2 px-6 rounded-full text-sm md:text-base hover:bg-[#1B6AB3] hover:text-white transition-colors duration-300 ease-in-out"
+      >
+        카카오로 로그인
+      </button>
+    </div>
   );
 }
